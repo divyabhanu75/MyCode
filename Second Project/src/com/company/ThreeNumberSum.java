3d87c3feb1c90cf97970ef95aee3d091884736ef
divyabhanu75 <divyabhanu75@gmail.com>
2020-05-15 15:16:36 -0500

    ThreeNumberSum

--- Second Project/src/com/company/ThreeNumberSum.java
+++ Second Project/src/com/company/ThreeNumberSum.java
@@ -4,7 +4,7 @@ public class ThreeNumberSum {
 
     public static void execute(String[] args) {
         int sum = 1+2+3 ;
-        System.out.println(sum);
+        System.out.println(sum) ;
 
     }
 }
