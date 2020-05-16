d0e05de1172f9141db294e3d611f9c8180742498
divyabhanu75 <divyabhanu75@gmail.com>
2020-05-16 13:02:22 -0500

    FindVowel using string contains method

---
+++ Second Project/src/com/company/FindVowelSecondMethod.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class FindVowelSecondMethod {
+
+    public static void main(String[] args) {
+
+        //Program to find whether a character is vowel or consonant
+        //Step 1: use scanner and retrieve input charcter
+        //Step 2: use a boolen expression in a condition to check whether inpu character is vowel
+        //step 3: If vowel print its a vowel else print its a consonat
+        int count = 0;
+        String vowels = "aeiouAEIOU";
+        while (count < 5) {
+            System.out.println("please enter a alphabet");
+            Scanner scan = new Scanner(System.in);
+            char inputChar = scan.next().charAt(0);
+
+            if (vowels.contains(Character.toString(inputChar)) ) {
+                System.out.println("Its a vowel");
+            } else {
+                System.out.println("Its a consonant");
+            }
+            count++;
+        }
+        System.out.println("End of the Program");
+    }
+}
